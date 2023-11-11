bc479612286f17f9e0644671d044a3a71386ec8f
Ali RaZa <62483196+AliRaZa1121@users.noreply.github.com>
2023-11-11 15:53:39 +0400

    Update MicrosoftGraphServices.ts

--- MicrosoftGraphServices.ts
+++ MicrosoftGraphServices.ts
@@ -9,7 +9,7 @@ export class MicrosoftGraphServices {
     private static clientId: string = process.env.CLIENT_ID || 'your-client-id';
     private static tenantId: string = process.env.TENANT_ID || 'your-tenant-id';
     private static SCOPE: string = "offline_access user.read mail.read";
-    private static REDIRECT_URI: string = process.env.REDIRECT_URI || "http://localhost/test_imap"; // Change as needed
+    private static REDIRECT_URI: string = process.env.REDIRECT_URI || "your redirect url"; // Change as needed
     private static clientSecretKey: string = process.env.CLIENT_SECRET_KEY || 'your-client-secret-key';
     private static clientSecretValue: string = process.env.CLIENT_SECRET_VALUE || 'your-client-secret-value';
 
@@ -26,11 +26,11 @@ export class MicrosoftGraphServices {
 
     public async getAuthenticationCodes(code: string): Promise<any> {
         try {
-            const response = await axios.post('https://login.microsoftonline.com/24b5276e-14d8-4880-a934-a57630d24fa5/oauth2/v2.0/token', {
+            const response = await axios.post(`https://login.microsoftonline.com/${MicrosoftGraphServices.tenantId}/oauth2/v2.0/token`, {
                 client_id: MicrosoftGraphServices.clientId,
                 scope: 'User.Read Mail.Read',
                 code,
-                redirect_uri: 'http://localhost/test_imap',
+                redirect_uri: MicrosoftGraphServices.REDIRECT_URI,
                 grant_type: 'authorization_code',
                 client_secret: MicrosoftGraphServices.clientSecretValue,
             });
